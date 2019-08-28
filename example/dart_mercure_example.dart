a764c7b3463917894a5fd9fb5fdfb6e3c6606e53
Wallerand Delevacq <wallforfry@gmail.com>
2019-08-28 19:13:40 +0200

    Clean code with dartfmt

--- example/dart_mercure_example.dart
+++ example/dart_mercure_example.dart
@@ -1,26 +1,29 @@
 import 'package:dart_mercure/dart_mercure.dart';
 
 main() {
-
   // Token generate with "mercure" JWT_KEY
-  String token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtZXJjdXJlIjp7InN1YnNjcmliZSI6WyIqIl0sInB1Ymxpc2giOlsiKiJdfX0.Gfj3BFrSOFhyFN0hPOYg-Pe4dfYWOKq7QJiMWYC2Z_k";
+  String token =
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtZXJjdXJlIjp7InN1YnNjcmliZSI6WyIqIl0sInB1Ymxpc2giOlsiKiJdfX0.Gfj3BFrSOFhyFN0hPOYg-Pe4dfYWOKq7QJiMWYC2Z_k";
   String hub_url = "http://mercure/hub";
   String topic = "http://mercure/test";
 
   Mercure mercure = Mercure(token: token, hub_url: hub_url);
-  mercure.subscribeTopics(topics: <String> [topic, "2"], onData: (Event event) {
-    print(event.data);
-  });
+  mercure.subscribeTopics(
+      topics: <String>[topic, "2"],
+      onData: (Event event) {
+        print(event.data);
+      });
 
-  mercure.subscribeTopic(topic: topic, onData: (Event event) {
-    print(event.data);
-  });
+  mercure.subscribeTopic(
+      topic: topic,
+      onData: (Event event) {
+        print(event.data);
+      });
 
   mercure.publish(topic: topic, data: "DATA").then((status) {
-    if(status == 200) {
+    if (status == 200) {
       print("Message Sent");
-    }
-    else {
+    } else {
       print('Message Failed with code : $status');
     }
   });
--- test/dart_mercure_test.dart
+++ test/dart_mercure_test.dart
@@ -3,7 +3,7 @@ import 'package:test/test.dart';
 
 void main() {
   group('Test mercure package', () {
-    Mercure mercure; 
+    Mercure mercure;
     Mercure mercure_bad_token;
     String token;
     String hub_url;
@@ -11,30 +11,33 @@ void main() {
 
     setUp(() {
       topic = "http://mercure/test";
-      token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtZXJjdXJlIjp7InN1YnNjcmliZSI6WyIqIl0sInB1Ymxpc2giOlsiKiJdfX0.Gfj3BFrSOFhyFN0hPOYg-Pe4dfYWOKq7QJiMWYC2Z_k";
+      token =
+          "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtZXJjdXJlIjp7InN1YnNjcmliZSI6WyIqIl0sInB1Ymxpc2giOlsiKiJdfX0.Gfj3BFrSOFhyFN0hPOYg-Pe4dfYWOKq7QJiMWYC2Z_k";
       hub_url = "http://mercure/hub";
       mercure = Mercure(token: token, hub_url: hub_url);
     });
 
     test('Subscribe Topic', () {
       String value = "NODATA";
-      mercure.subscribeTopic(topic: topic, onData: (Event event) {
-         value = event.data; 
-      });
+      mercure.subscribeTopic(
+          topic: topic,
+          onData: (Event event) {
+            value = event.data;
+          });
       Future.delayed(const Duration(seconds: 1), () {
         mercure.publish(topic: topic, data: "DATA").then((_) {
-          expect(value, "DATA");    
+          expect(value, "DATA");
         });
       });
     });
-    
+
     test('Test Publish', () async {
-      expect(await mercure.publish(topic: topic, data: "DATA"), 200);  
+      expect(await mercure.publish(topic: topic, data: "DATA"), 200);
     });
-    
+
     test('Test Publish bad token', () async {
-      mercure_bad_token = Mercure(token: token+"A", hub_url: hub_url);
-      expect(await mercure_bad_token.publish(topic: topic, data: "DATA"), 401);  
+      mercure_bad_token = Mercure(token: token + "A", hub_url: hub_url);
+      expect(await mercure_bad_token.publish(topic: topic, data: "DATA"), 401);
     });
   });
 }
