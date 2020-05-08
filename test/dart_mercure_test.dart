e1d7ebe6e1748d5227f87e13fa4c03fdba544812
Christian Alexander Wolf <mail@cawolf.de>
2020-05-08 22:06:18 +0200

    feat: setup test workflow on github

--- test/dart_mercure_test.dart
+++ test/dart_mercure_test.dart
@@ -10,34 +10,34 @@ void main() {
     String topic;
 
     setUp(() {
-      topic = "http://mercure/test";
+      topic = 'https://mercure/test';
       token =
-          "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtZXJjdXJlIjp7InN1YnNjcmliZSI6WyIqIl0sInB1Ymxpc2giOlsiKiJdfX0.Gfj3BFrSOFhyFN0hPOYg-Pe4dfYWOKq7QJiMWYC2Z_k";
-      hub_url = "http://mercure/hub";
+          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJtZXJjdXJlIjp7InN1YnNjcmliZSI6WyIqIl0sInB1Ymxpc2giOlsiKiJdfX0.YIcYqBdnLwiweTIo6my83ryFyBNKxJ8m5Cy426Du3WI';
+      hub_url = 'http://mercure/.well-known/mercure';
       mercure = Mercure(token: token, hub_url: hub_url);
     });
 
     test('Subscribe Topic', () {
-      String value = "NODATA";
+      var value = 'NODATA';
       mercure.subscribeTopic(
           topic: topic,
           onData: (Event event) {
             value = event.data;
           });
       Future.delayed(const Duration(seconds: 1), () {
-        mercure.publish(topic: topic, data: "DATA").then((_) {
-          expect(value, "DATA");
+        mercure.publish(topic: topic, data: 'DATA').then((_) {
+          expect(value, 'DATA');
         });
       });
     });
 
     test('Test Publish', () async {
-      expect(await mercure.publish(topic: topic, data: "DATA"), 200);
+      expect(await mercure.publish(topic: topic, data: 'DATA'), 200);
     });
 
     test('Test Publish bad token', () async {
-      mercure_bad_token = Mercure(token: token + "A", hub_url: hub_url);
-      expect(await mercure_bad_token.publish(topic: topic, data: "DATA"), 401);
+      mercure_bad_token = Mercure(token: token + 'A', hub_url: hub_url);
+      expect(await mercure_bad_token.publish(topic: topic, data: 'DATA'), 401);
     });
   });
 }
